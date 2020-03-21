87856d361785bb331e699d749a052888efccf239
Kieran Levin <ktl@frame.work>
2020-03-20 19:03:53 -0700

    cleanup formatting

--- samples/boards/stm32/stm32g071rb_disco/README.rst
+++ samples/boards/stm32/stm32g071rb_disco/README.rst
@@ -5,17 +5,17 @@ STM32 Go Discovery Kit USB-C Analyzer
 
 Overview
 ********
-Show usage of the discovery kit, which is primarily used as a PD analyzer and usb-c PD development platform. 
-The discovery kit has a built in ST-LINK programmer, and a virtual com port usb cdc to uart serial terminal 
+Show usage of the discovery kit, which is primarily used as a PD analyzer and usb-c PD development platform.
+The discovery kit has a built in ST-LINK programmer, and a virtual com port usb cdc to uart serial terminal
 that connects to the stm32g071rb UART 3.
 Other devices on the system include:
- * Captive USB-C cable with CC lines connected to the STM32's USBPD1 controller. 
- * USB-C port passthrough. 
- * 3 I2C INA230 voltage/current/power sensors connected to usb-C Vbus and CC1, CC2 lines. 
+ * Captive USB-C cable with CC lines connected to the STM32's USBPD1 controller.
+ * USB-C port passthrough.
+ * 3 I2C INA230 voltage/current/power sensors connected to usb-C Vbus and CC1, CC2 lines.
  * 4 LEDS
- * 4-way Joystick 
+ * 4-way Joystick
  * 0.96in 128x64 OLED Display with a SSD1315Z controller
- * 32khz RTC oscillator. 
+ * 32khz RTC oscillator.
 
 
 Building and Running
--- samples/boards/stm32/stm32g071rb_disco/src/main.c
+++ samples/boards/stm32/stm32g071rb_disco/src/main.c
@@ -13,25 +13,25 @@
 
 void main(void)
 {
-        struct device *dev;
-        bool led_is_on = true;
-        int ret;
+	struct device *dev;
+	bool led_is_on = true;
+	int ret;
 
-        dev = device_get_binding(DT_ALIAS_LED0_GPIOS_CONTROLLER);
-        if (dev == NULL) {
-                return;
-        }
+	dev = device_get_binding(DT_ALIAS_LED0_GPIOS_CONTROLLER);
+	if (dev == NULL) {
+			return;
+	}
 
-        ret = gpio_pin_configure(dev, DT_ALIAS_LED0_GPIOS_PIN,
-                                 GPIO_OUTPUT_ACTIVE
-                                 | DT_ALIAS_LED0_GPIOS_FLAGS);
-        if (ret < 0) {
-                return;
-        }
-        /* initial placeholder, blink LED */ 
-        while (1) {
-                gpio_pin_set(dev, DT_ALIAS_LED0_GPIOS_PIN, (int)led_is_on);
-                led_is_on = !led_is_on;
-                k_sleep(SLEEP_TIME_MS);
-        }
+	ret = gpio_pin_configure(dev, DT_ALIAS_LED0_GPIOS_PIN,
+								GPIO_OUTPUT_ACTIVE
+								| DT_ALIAS_LED0_GPIOS_FLAGS);
+	if (ret < 0) {
+			return;
+	}
+	/* initial placeholder, blink LED */
+	while (1) {
+			gpio_pin_set(dev, DT_ALIAS_LED0_GPIOS_PIN, (int)led_is_on);
+			led_is_on = !led_is_on;
+			k_sleep(SLEEP_TIME_MS);
+	}
 }
